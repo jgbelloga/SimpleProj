d79d92cadf1c8a218046208e0154967108f90e86
jgbelloga <jgbelloga3380@eagle.fgcu.edu>
2017-10-23 18:25:25 -0400

    //simple multiplication loop

--- SimpleProject/src/simple/Simple.java
+++ SimpleProject/src/simple/Simple.java
@@ -1,10 +1,20 @@
 package simple;
 
+import java.util.Scanner;
+
 public class Simple {
 
 	
 	public static void main(String args[]) {
-		System.out.println("new");
+		
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int result = 0;
+		for(int i = 0; i < 10; i++) {
+			
+			result = n * (i + 1);
+			System.out.println(n + " x " + (i + 1) + " = " + result);
+		}
 		
 	}
 }
